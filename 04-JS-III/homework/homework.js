7ab0604df0b681db095c1aa70ca63e5ebac13171
marcekan <mateojose328@gmail.com>
2021-10-28 21:43:54 -0300

    finish homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -2,19 +2,22 @@
 
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
-  // Tu código:
+  // Tu código: 
+  return (array [0])
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return (array [array.length -1])
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return (array.length)
 }
 
 
@@ -22,14 +25,18 @@ function incrementarPorUno(array) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
-  // Tu código:
-}
+  // Tu código: 
+  for (var i = 0; i < array.length; i++) {
+    nuevoArray[i] = array[i] + 1;
+      return nuevoArray;
+  }
+  }
 
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
-  // Tu código:
+  // Tu código: 
 }
 
 
@@ -37,7 +44,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // Añade el "elemento" al comienzo del array
   // y devuelve el array
   // Pista: usa el método `.unshift`
-  // Tu código:
+  // Tu código: 
+
 }
 
 
